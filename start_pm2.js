8a6aab54f345f6270b8ce36d13a7e1aae1a15e54
terrenceliao <yuzhang.liao@mail.utoronto.ca>
2023-06-11 19:30:08 -0400

    update

--- start_pm2.js
+++ start_pm2.js
@@ -2,7 +2,7 @@ import schedule from 'node-schedule'
 import cronTime from './config/server-config.js'
 import mainForProd from './main.js'
 
-console.log('======【定时推送服务已启动, enjoying it】======!!!!')
+console.log('======【定时推送服务已启动, enjoying it】====?=!!!!')
 console.log(`目前定时推送的配置为：【${cronTime}】`)
 schedule.scheduleJob(cronTime, () => {
   mainForProd()
