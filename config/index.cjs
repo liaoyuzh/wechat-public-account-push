9cf55771baa812869bd0d8dc1f2b70d802382214
terrenceliao <yuzhang.liao@mail.utoronto.ca>
2023-06-11 21:19:50 -0400

    updated id

--- config/index.cjs
+++ config/index.cjs
@@ -26,7 +26,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'ChronoWalker',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: this.USER_ID,
+      id: 'oFOXb6QeNi5FKGYHK9guf-9i71AU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: this.TEMPLATE_ID,
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
