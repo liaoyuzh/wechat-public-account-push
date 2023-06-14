5ef3b081a566ba68d66986b779c35fcf7246260f
terrenceliao <yuzhang.liao@mail.utoronto.ca>
2023-06-14 12:20:48 -0400

    11

--- config/index.cjs
+++ config/index.cjs
@@ -70,7 +70,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'ChronoWalker',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oFOXb6QeNi5FKGYHK9guf-9i71AU',
+      id: 'oFOXb6bx9jeqj2l1cUZe8B5WRNH8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       //useTemplateId: 'hgMxhD_lRtWtjh0BwpdELf_C7VeWdrBBnUFAs9I3W1s',
       //useTemplateId: 'ZigcFRnS8Nmlz5hjTKRw9VIhFI_Q_gYFrMS5Pkl60ZY',
@@ -104,7 +104,8 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oFOXb6bx9jeqj2l1cUZe8B5WRNH8',
+      //id: 'oFOXb6bx9jeqj2l1cUZe8B5WRNH8',
+      id: 'oFOXb6QeNi5FKGYHK9guf-9i71AU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'F35GENy7EfoyxhWrhCj9QdcnGAm8-EwCSa6CBdUCDwU',
 
